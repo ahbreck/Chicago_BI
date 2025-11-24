12029bcb3806383022e2b0d472bae867e45ec7ad
Andrew Breck <31482159+ahbreck@users.noreply.github.com>
2025-11-23 20:24:15 -0500

    Update trip_reports.go

--- src/cmd/reports/trip_reports.go
+++ src/cmd/reports/trip_reports.go
@@ -192,8 +192,8 @@ func CreateCovidCategoryReport(db *sql.DB) error {
 			SELECT c.*, wt.week_start, SUM(wt.trips) AS weekly_trips
 			FROM %s c
 			JOIN weekly_trips wt ON wt.zip_code = c."community_area_or_zip"
-			WHERE UPPER(c."ccvi_category") = 'HIGH'
-				AND UPPER(c."geography_type") = 'ZIP'
+			WHERE UPPER(TRIM(c."ccvi_category")) = 'HIGH'
+				AND UPPER(TRIM(c."geography_type")) = 'ZIP'
 			GROUP BY c."id", c."geography_type", c."community_area_or_zip", c."community_area_name", c."ccvi_score", c."ccvi_category", wt.week_start`, CCVIIdent, alertsIdent, alertsIdent, ccviIdent),
 		fmt.Sprintf(`DROP TABLE IF EXISTS %s`, CCVISortedIdent),
 		fmt.Sprintf(`CREATE TABLE %s AS
